92326b49457313df55488f364dbef8e80074a90a
michaelorscheln <michaelorscheln@crimson.ua.edu>
2023-03-19 10:09:33 -0500

    add vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react';
-
 import { CSSReset, useColorModeValue } from '@chakra-ui/react';
 
 import themeattempt from "../styles/themeattempt";
 import { createRoot } from "react-dom/client";
 import MainLayout from '../components/MainLayout';
-
+import { Analytics } from '@vercel/analytics/react';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -21,16 +20,17 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-    
-    <ChakraProvider theme={themeattempt}>
-    
-      
-      <CSSReset />
-      
-      <MainLayout >
-        <Component {...pageProps} />
+
+      <ChakraProvider theme={themeattempt}>
+
+
+        <CSSReset />
+
+        <MainLayout >
+          <Component {...pageProps} />
+          <Analytics />
         </MainLayout>
-    </ChakraProvider>
+      </ChakraProvider>
     </>
   );
 }
